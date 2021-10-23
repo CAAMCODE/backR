4fa448de1d289f0944ff33a8a0cf3e28b07b4611
CAABCODE <ing.amaya.bautista@gmail.com>
2021-10-23 11:09:32 -0500

    backendR

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
-const express = require('express');
+const express = require("express");
 // import express from 'express'
-var morgan = require('morgan');
+var morgan = require("morgan");
 // import morgan from 'morgan';
-var cors = require('cors');
+var cors = require("cors");
 // import cors from 'cors';
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 // import mongoose from 'mongoose';
-const router = require('./routes');
+const router = require("./routes");
 // import apiRouter from './routes/index';
 
 //conexión a la base de datos
 // const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const urlDB = 'mongodb://localhost:27017/dbsistema';
-mongoose.connect(urlDB, {
+const urlDB =
+  "mongodb+srv://recuperacion_ciclo3_user:Gc7BN9XzRVxDM0Sh@cluster0.mmaa8.mongodb.net/recuperacionDB?retryWrites=true&w=majority";
+mongoose
+  .connect(urlDB, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(mongoose => console.log('Conectado on 27017'))
-.catch(err => console.log(err))
+    useUnifiedTopology: true,
+  })
+  .then((mongoose) => console.log("Conectado con Atlas"))
+  .catch((err) => console.log(err));
 
 const app = express();
-app.use(morgan('dev'));
-app.use(cors())
+app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 //rutas
-app.use('/api', router);
+app.use("/api", router);
 
-app.set('PORT', process.env.PORT || 3000);
+app.set("PORT", process.env.PORT || 3000);
 
 //middleware
 
-
-
-app.listen(app.get('PORT'), () => {
-  console.log(`Server started on port: ${app.get('PORT')}`);
+app.listen(app.get("PORT"), () => {
+  console.log(`Server started on port: ${app.get("PORT")}`);
 });
 
-app.get('/', (req,res) => {
-    res.send('Hello World');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
